c674a2f3b83507bbd11c9593e4fe27ea766529a0
khadijaumud <kh.umud07@gmail.com>
2026-07-19 18:23:04 +0000

    add task

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+function handleResponseFromAPI(promise) {
+    return (
+      promise
+        .then(() => ({
+          status: 200,
+          body: 'success',
+        }))
+        .catch(() => Error())
+        .finally(() => {
+          console.warn('Got a response from the API');
+        })
+    );
+  }
+  
+  export default handleResponseFromAPI;
